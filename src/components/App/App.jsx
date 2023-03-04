530a3097c829f68c9defbcf1985e54a6abb72a7b
Olesia Andreiko <olesikmail@gmail.com>
2023-03-04 11:13:38 +0200

    refactoring by hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
 
 import {
@@ -12,56 +12,41 @@ import {
   Todo,
 } from 'components';
 
-export class App extends Component {
-  state = {
-    todos: [],
-  };
+export const App =() => {
 
-  componentDidMount() {
-    const todos = JSON.parse(localStorage.getItem('todos'));
+  const [todos, setTodos]= useState(() => JSON.parse(localStorage.getItem('todos')) || []);
 
-    if (todos) {
-      this.setState(() => ({ todos }));
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    const { todos } = this.state;
 
-    if (prevState.todos !== todos) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }
+ useEffect(()=> {
+  localStorage.setItem('todos', JSON.stringify(todos));
+ }, [todos])
 
-  addTodo = text => {
+
+  const addTodo = text => {
     const todo = {
       id: nanoid(),
       text,
     };
 
-    this.setState(({ todos }) => ({
-      todos: [...todos, todo],
-    }));
+    setTodos(( prevTodos ) => ([...prevTodos, todo]));
   };
 
-  handleSubmit = data => {
-    this.addTodo(data);
+  const handleSubmit = data => {
+    addTodo(data);
   };
 
-  deleteTodo = id => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== id),
-    }));
+  const deleteTodo = id => {
+    setTodos(prevState => (
+      prevState.filter(todo => todo.id !== id)
+    ));
   };
-
-  render() {
-    const { todos } = this.state;
 
     return (
       <>
         <Header />
         <Section>
           <Container>
-            <SearchForm onSubmit={this.handleSubmit} />
+            <SearchForm onSubmit={handleSubmit} />
 
             {todos.length === 0 && (
               <Text textAlign="center">There are no any todos ... </Text>
@@ -75,7 +60,7 @@ export class App extends Component {
                       id={todo.id}
                       text={todo.text}
                       counter={index + 1}
-                      onClick={this.deleteTodo}
+                      onClick={deleteTodo}
                     />
                   </GridItem>
                 ))}
@@ -84,5 +69,5 @@ export class App extends Component {
         </Section>
       </>
     );
-  }
+  
 }
